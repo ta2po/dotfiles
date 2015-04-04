0765678ec98d1cdbed3a1cbb9b20b963cd5caf7e
ta2po <zeroAnotice@gmail.com>
2015-04-05 00:03:39 +0900

    [add] ignore and making init dir script

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -sf ~/dotfiles/_vimrc ~/.vimrc
+ln -sf ~/dotfiles/colors ~/.vim
+#ln -sf ~/dotfiles/ftdetect ~/.vim
+#ln -sf ~/dotfiles/indent ~/.vim
+#ln -sf ~/dotfiles/_bash_profile ~/.bash_profile
+#ln -sf ~/dotfiles/_wgetrc ~/.wgetrc
+#ln -sf ~/dotfiles/Brewfile ~/Brewfile
